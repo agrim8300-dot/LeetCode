23341cba0e76bf6824c7afe02b73416aabd6db4a
agrim8300-dot <agrim8300@gmail.com>
2026-07-27 10:43:39 +0530

    Time: 115 ms (8.14%) | Memory: 17.9 MB (64.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
-        vector<int> ans;
+        unordered_map<int ,int> mp;
         vector<int> copy;
         
-        
-        for(int i = 0; i < nums.size(); i++)  
+        vector<int>ans;
+        for(int i=0 ; i<nums.size() ; i++)
         {
-            if(m.find(nums[i]) == m.end()){
+            if(mp.find(nums[i]) == mp.end())
+            {
                 copy.push_back(nums[i]);
-                m[nums[i]] = 0;
             }
-            m[nums[i]]++; 
+            mp[nums[i]]++;
         }
-         int n = copy.size();
-        for(int i = 0; i < n - 1; i++){ // m - 1 iter.. har iter.. pe ek elem sort.
+        int n=copy.size();
+        //bubble sort to sort the frequencies in decreasing order
+        for(int i=0 ; i<n-1 ; i++)
+        {
             bool flag = true;
-            for(int j = 0; j < n - i - 1; j++){ // total comparison , decre.. starting with m - 1;
-                if(m[copy[j]] > m[copy[j + 1]]){ // customization.
+            for(int j=0 ; j<n-i-1 ; j++)
+            {
+                if(mp[copy[j]] > mp[copy[j+1]])
+                {
                     flag = false;
-                    swap(copy[j] , copy[j + 1]);
+                    swap(copy[j],copy[j+1]);
                 }
+                
             }
-            if(flag == true){
-                break;
+            if(flag) break;
+        }
+            int val=0;
+            int idx = n-1;
+            while(val < k)
+            {
+                ans.push_back(copy[idx]);
+                idx--;
+                val++;
             }
-        }
-        int val = 0;
-        int idx = n - 1;
-        while(val < k){
-          ans.push_back(copy[idx]);
-          idx--;
-          val++;
-        }
         
         return ans;
     }
