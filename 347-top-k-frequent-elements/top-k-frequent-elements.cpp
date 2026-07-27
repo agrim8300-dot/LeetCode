ce2c752365b558e200814c90fc1b0fdcf46e0fd7
agrim8300-dot <agrim8300@gmail.com>
2026-07-27 11:39:24 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (40.16%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int ,int> mp;
-        vector<int> copy;
-        
-        vector<int>ans;
+        unordered_map<int,int> mp;
+
         for(int i=0 ; i<nums.size() ; i++)
         {
-            if(mp.find(nums[i]) == mp.end())
-            {
-                copy.push_back(nums[i]);
-            }
             mp[nums[i]]++;
         }
-        int n=copy.size();
-        //bubble sort to sort the frequencies in decreasing order
-        for(int i=0 ; i<n-1 ; i++)
+
+        priority_queue<pair<int,int>> heap;
+
+        for(auto& it: mp)
         {
-            bool flag = true;
-            for(int j=0 ; j<n-i-1 ; j++)
-            {
-                if(mp[copy[j]] > mp[copy[j+1]])
-                {
-                    flag = false;
-                    swap(copy[j],copy[j+1]);
-                }
-                
-            }
-            if(flag) break;
+            heap.push({it.second,it.first});
         }
-            int val=0;
-            int idx = n-1;
-            while(val < k)
-            {
-                ans.push_back(copy[idx]);
-                idx--;
-                val++;
-            }
-        
+        vector<int>ans;
+        while(k--)
+        {
+            ans.push_back(heap.top().second);
+            heap.pop();
+        }
         return ans;
     }
 };
