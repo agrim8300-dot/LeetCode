a0fe605aa5a2fe1b6ad777bc9e07aba3d3f84594
agrim8300-dot <agrim8300@gmail.com>
2026-08-03 14:27:28 +0530

    Time: 4 ms (47.77%) | Memory: 12.1 MB (28.24%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        unordered_set<int> set(nums.begin(),nums.end());
-        int count=0;
-        for(auto& x:nums)
+        unordered_map<int, int> mp;
+
+        for(int x: nums)
         {
-            if( set.count(x+diff) &&    set.count(x+2*diff) ){
+            mp[x]++;
+        }
+        int count=0;
+
+        for(int x:nums)
+        {
+            if(mp.find(x+diff) != mp.end() && mp.find(x+2*diff)!= mp.end())
+            {
                 count++;
             }
         }
