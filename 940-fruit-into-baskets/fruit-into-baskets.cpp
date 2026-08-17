a8741e7221c28df5df79c09e851e4e1790a653b2
agrim8300-dot <agrim8300@gmail.com>
2026-08-17 11:55:24 +0530

    Time: 51 ms (27.67%) | Memory: 84.8 MB (84.35%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> mp;
+        int left = 0;
+        int maxfruits = 0;
+        
+        for (int right = 0; right < fruits.size(); right++) {
+        
+            mp[fruits[right]]++;
+            
+            // If more than 2 types shrink from left...
+            while (mp.size() > 2) {
+                mp[fruits[left]]--;
+                if (mp[fruits[left]] == 0) {
+                    mp.erase(fruits[left]);
+                }
+                left++;
+            }
+            
+            
+            maxfruits = max(maxfruits, right - left + 1);
+        }
+        
+        return maxfruits;
+    }
+};
