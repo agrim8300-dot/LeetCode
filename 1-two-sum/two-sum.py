cc80b14af11f9261c32cbe37b4ea0b81be644928
agrim8300-dot <agrim8300@gmail.com>
2026-08-27 01:51:50 +0530

    Time: 1713 ms (29.38%) | Memory: 19.9 MB (66.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,6 +4,7 @@ class Solution:
         for i in range(n-1):
             for j in range(i+1 , n):
                 if(nums[i]+nums[j] == target):
+          
                     return [i,j]
 
         return []     
