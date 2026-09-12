1c64a02ad80eca71236193443e91b6bdac0fd621
agrim8300-dot <agrim8300@gmail.com>
2026-09-12 18:38:11 +0530

    Time: 3 ms (25.86%) | Memory: 17.3 MB (15.79%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -15,19 +15,18 @@ public:
         queue<TreeNode*> q;
         vector<vector<int>> ans;
         
-        if(root == NULL) return ans;
+        if(root == NULL ) return ans;
         q.push(root);
-        
         while(!q.empty())
         {
-            int n=q.size();
             vector<int> res;
-            for(int i=0 ; i<n ; i++)
+            int n=q.size();
+            for(int i=0 ; i<n;i++)
             {
-                TreeNode* node =  q.front();
+                TreeNode* node = q.front();
                 q.pop();
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
+                if(node->left!=NULL) q.push(node->left);
+                if(node->right!=NULL)q.push(node->right);
                 res.push_back(node->val);
             }
             ans.push_back(res);
